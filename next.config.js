6e049d29eb95928c639795f098c01a04f54b0e63
vtarapara <vtara321@gmail.com>
2025-07-21 01:57:10 -0400

    next.js config

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  basePath: process.env.NODE_ENV === "production" ? "/your-repo-name" : "",
+  assetPrefix: process.env.NODE_ENV === "production" ? "/your-repo-name/" : "",
+}
+
+module.exports = nextConfig
